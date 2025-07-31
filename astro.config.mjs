4ea78f31221266dc84c2b183a2e137ead8876780
NotNickMoorman <nicholasdmoorman@gmail.com>
2025-07-30 23:24:38 -0400

    added prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import alpine from "@astrojs/alpinejs";
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import alpine from '@astrojs/alpinejs'
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpine()],
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
